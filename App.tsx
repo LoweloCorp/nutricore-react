27a90e60707097cd3edbc5a023fe35b08c7d49d9
Tobias Barthold <tobias-barthold@outlook.de>
2024-03-02 14:44:06 +0100

    fix tests

--- App.tsx
+++ App.tsx
@@ -3,6 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
 import Navigation from "./src/components/navigation";
 import {PaperProvider} from "react-native-paper";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+import schema from "./src/data/schema";
+import {Database} from "@nozbe/watermelondb";
+import {Product} from "./src/data/models/product";
+import {Nutriment} from "./src/data/models/nutriment";
+import {Nutritional_log} from "./src/data/models/nutritional_log";
+import {User} from "./src/data/models/user";
 
 export default function App() {
     return (
@@ -20,3 +27,26 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+const adapter = new SQLiteAdapter({
+    schema,
+    // (You might want to comment it out for development purposes -- see Migrations documentation)
+    // migrations,
+    // (optional database name or file system path)
+    // dbName: 'myapp',
+    // (recommended option, should work flawlessly out of the box on iOS. On Android,
+    // additional installation steps have to be taken - disable if you run into issues...)
+    jsi: true, /* Platform.OS === 'ios' */
+    // (optional, but you should implement this method)
+    onSetUpError: error => {
+        // Database failed to load -- offer the user to reload the app or log out
+    }
+})
+
+// Then, make a Watermelon database from it!
+export const database = new Database({
+    adapter,
+    modelClasses: [
+        Product, Nutriment, Nutritional_log, User
+    ],
+})
